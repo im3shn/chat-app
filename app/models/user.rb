f2eae73d74b26e48a5b326631fdd4d14e8166f08
im3shn <im3shn@gmail.com>
2023-09-30 17:36:51 +0530

    modifying default url host

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_many :messages, dependent: :destroy
   has_many :conversations, foreign_key: :sender_id
-
+  validates :first_name, presence: :true
+  validates :last_name, presence: :true
   def self.full_name(current_user="")
   	if current_user.present?
 	  current_user.first_name + " " + current_user.last_name
